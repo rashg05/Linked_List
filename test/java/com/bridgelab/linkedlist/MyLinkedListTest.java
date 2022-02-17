acf4fffed5229d317dcde4fce124e25bc175de4f
rashg05 <gajra.rashmi0504@gmail.com>
2022-02-17 21:12:20 +0530

    Added: simple linked list program to add data in nodes Ability to create Linked

---
+++ test/java/com/bridgelab/linkedlist/MyLinkedListTest.java
@@ -0,0 +1,22 @@
+package com.bridgelab.linkedlist;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MyLinkedListTest {
+	@Test
+	public void given3NumberWhenAddedToLinkedShouldBeAddedToTop() {
+		MyNode<Integer> myFirstNode= new MyNode<>(70);
+		MyNode<Integer> mySecondNode= new MyNode<>(30);
+		MyNode<Integer> myThirdNode= new MyNode<>(56);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		myLinkedList.printMyNode();
+		boolean result = myLinkedList.head.equals(myThirdNode) &&
+						 myLinkedList.head.getNext().equals(mySecondNode) &&
+						 myLinkedList.tail.equals(myFirstNode);
+		Assert.assertTrue(result);
+	}
+}
